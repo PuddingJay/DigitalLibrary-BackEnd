358aa2cb62d56aa6cc5443c56c594d2ff3fd3ea2
PuddingJay <muhammadwindu27@gmail.com>
2023-07-07 16:14:38 +0700

    inc & dec 'tersedia' on Peminjaman create & del

---
+++ config/model/adminModel.js
@@ -0,0 +1,19 @@
+import Sequelize from "sequelize";
+import db from '../database/db.js';
+
+
+const admin = db.define(
+  "admin",
+  {
+    name: Sequelize.STRING,
+    username: Sequelize.STRING,
+    password: Sequelize.STRING,
+    refreshToken: Sequelize.TEXT,
+  },
+  {
+    freezeTableName: true,
+    timestamps: false,
+  }
+);
+
+export default admin;
---
+++ config/model/booksModel.js
@@ -0,0 +1,27 @@
+import Sequelize from "sequelize";
+import db from "../database/db.js";
+
+const books = db.define(
+  "books",
+  {
+    idBuku: Sequelize.INTEGER,
+    kodeBuku: { type: Sequelize.INTEGER, primaryKey: true },
+    judul: Sequelize.STRING,
+    penulis: Sequelize.STRING,
+    Kategori: Sequelize.STRING,
+    tahun_terbit: Sequelize.STRING,
+    keterangan: Sequelize.STRING,
+    jumlah: Sequelize.INTEGER,
+    tersedia: Sequelize.INTEGER,
+    cover_buku: Sequelize.STRING,
+    file_ebook: Sequelize.STRING,
+  },
+  {
+    freezeTableName: true,
+    timestamps: false,
+  }
+);
+
+books.removeAttribute("id");
+
+export default books;
