2405c63edfcca2904dc0051457fbaa239024a992
Shofiyullah05 <71362729+Shofiyullah05@users.noreply.github.com>
2023-08-29 00:29:46 +0700

    Add files via upload

---
+++ Config/model/booksModel.js
@@ -0,0 +1,29 @@
+const Sequelize = require('sequelize')
+const db = require('../database/db.js')
+
+const books = db.define(
+  'books',
+  {
+    kodeBuku: { type: Sequelize.INTEGER, primaryKey: true },
+    judul: Sequelize.STRING,
+    penulis: Sequelize.STRING,
+    Kategori: Sequelize.STRING,
+    ringkasan: Sequelize.STRING,
+    tahun_terbit: Sequelize.STRING,
+    keterangan: Sequelize.STRING,
+    jumlah: Sequelize.INTEGER,
+    tersedia: Sequelize.INTEGER,
+    cover_buku: Sequelize.STRING,
+    file_ebook: Sequelize.STRING,
+    likes: Sequelize.INTEGER,
+    isApproval: Sequelize.STRING,
+  },
+  {
+    freezeTableName: true,
+    timestamps: false,
+  },
+)
+
+books.removeAttribute('id')
+
+module.exports = books
---
+++ Config/model/siswaModel.js
@@ -0,0 +1,24 @@
+const Sequelize = require('sequelize')
+const db = require('../database/db.js')
+
+const siswa = db.define(
+  'siswa',
+  {
+    NIS: { type: Sequelize.INTEGER, primaryKey: true },
+    Nama: Sequelize.STRING,
+    password: Sequelize.TEXT,
+    Kelas: Sequelize.STRING,
+    Jurusan: Sequelize.STRING,
+    jumlahPinjam: Sequelize.INTEGER,
+    waktuPinjam: Sequelize.DATE,
+    refreshToken: Sequelize.TEXT,
+  },
+  {
+    freezeTableName: true,
+    timestamps: false,
+  },
+)
+
+siswa.removeAttribute('id')
+
+module.exports = siswa
